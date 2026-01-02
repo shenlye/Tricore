9f378fb0d5573828f1c5b1fd3a6867fa34d7b14e
Shenley <582783985@qq.com>
2026-01-02 16:19:01 +0800

    feat: implement user authentication with login route and JWT support (#7)
    * feat: implement user authentication with login route and JWT support
    
    * feat: improve login handler to handle user not found and password verification
    
    * feat: add JWT_SECRET validation and improve login schema formatting

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,19 @@
 import { OpenAPIHono } from "@hono/zod-openapi";
 import { Scalar } from "@scalar/hono-api-reference";
 import { seedDefaultUser } from "./db";
+import authRouter from "./routes/auth/index";
 import postsRouter from "./routes/posts/index";
 
 const app = new OpenAPIHono();
 
 await seedDefaultUser();
+
+const jwtSecret = process.env.JWT_SECRET;
+
+if (!jwtSecret) {
+    console.error("ERROR: JWT_SECRET is missing in environment variables.");
+    process.exit(1);
+}
 
 app.get("/", (c) => {
     return c.text("Hello Hono!");
@@ -27,5 +35,6 @@ app.doc("/doc", {
 app.get("/scalar", Scalar({ url: "/doc" }));
 
 app.route("/posts", postsRouter);
+app.route("/auth", authRouter);
 
 export default app;
---
+++ src/routes/auth/handlers.ts
@@ -0,0 +1,44 @@
+import type { RouteHandler } from "@hono/zod-openapi";
+import { eq, or } from "drizzle-orm";
+import { sign } from "hono/jwt";
+import { db } from "../../db";
+import { users } from "../../db/schema";
+import type { loginRoute } from "./routers";
+
+export const loginHandler: RouteHandler<typeof loginRoute> = async (c) => {
+    const { identifier, password } = c.req.valid("json");
+
+    const user = await db
+        .select()
+        .from(users)
+        .where(or(eq(users.username, identifier), eq(users.email, identifier)))
+        .limit(1);
+
+    const foundUser = user[0];
+
+    const dummyHash =
+        "$argon2id$v=19$m=65536,t=3,p=4$c29tZXNhbHQ$q/v5V4AmI3f23aVw7V7d2A";
+    const passwordHash = foundUser ? foundUser.passwordHash : dummyHash;
+    const isMatch = await Bun.password.verify(passwordHash, password);
+
+    if (!foundUser || !isMatch) {
+        return c.json({ message: "Invalid username/email or password" }, 401);
+    }
+
+    const payload = {
+        sub: user[0].id,
+        role: user[0].role,
+        exp: Math.floor(Date.now() / 1000) + 60 * 60 * 24 * 7,
+    };
+
+    const jwtSecret = process.env.JWT_SECRET;
+
+    if (!jwtSecret) {
+        console.error("ERROR: JWT_SECRET is not set.");
+        return c.json({ message: "Internal server error" }, 500);
+    }
+
+    const token = await sign(payload, jwtSecret);
+
+    return c.json({ token: token }, 200);
+};
